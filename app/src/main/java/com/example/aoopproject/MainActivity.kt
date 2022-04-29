f7419217fa846eb2e92fc4db59333f1359907287
Dries1234 <driesolbrechts@gmail.com>
2022-04-29 10:09:11 +0200

    menu

--- app/src/main/java/com/example/aoopproject/MainActivity.kt
+++ app/src/main/java/com/example/aoopproject/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.aoopproject
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.zxing.integration.android.IntentIntegrator
+import androidx.drawerlayout.widget.DrawerLayout
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+
 
 val DEBUG: Boolean = true
 val CODE: String = "3161711001971"
@@ -18,11 +23,31 @@ val CODE: String = "3161711001971"
 class MainActivity : AppCompatActivity() {
     lateinit var camera: Button;
     lateinit var textView: TextView;
+    lateinit var drawerLayout: DrawerLayout;
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle;
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer);
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
         camera = findViewById<Button>(R.id.camerabutton)
 
         if (!DEBUG) {
@@ -63,4 +88,5 @@ class MainActivity : AppCompatActivity() {
         productView.putExtra("code", CODE)
         startActivity(productView)
     }
+
 }
